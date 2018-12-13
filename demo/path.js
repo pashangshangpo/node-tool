bad61037309a279323496d201612da486946b1a1
爬上上坡 <pashangshangpo@gmail.com>
2018-12-13 21:58:07 +0800

    test: 新增 Path.exists测试

--- demo/path.js
+++ demo/path.js
@@ -1,5 +1,14 @@
 const { Path } = require('../dist/node-tool.cjs')
 
-console.log(Path.JoinApp('package.json'))
-console.log(Path.JoinApp('./package.json'))
-console.log(Path.JoinApp('/package.json'))
+console.log(Path.joinApp('package.json'))
+console.log(Path.joinApp('./package.json'))
+console.log(Path.joinApp('/package.json'))
+
+
+Path.exists(Path.joinApp('package.json')).then(res => {
+  console.log(res)
+})
+
+Path.exists(Path.joinApp('package.json1')).then(res => {
+  console.log(res)
+})
